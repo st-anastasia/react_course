f0e1d4db9c38c6fc306ca40dbda26c27e915c893
stan <anastasia.stieb@gmail.com>
2017-10-23 22:22:57 +0200

    show video list

---
+++ src/components/video_list_item.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const VideoListItem = ({video}) => {
+    //const video = props.video; // the same in ES6 the next line
+    const imageURL = video.snippet.thumbnails.default.url;
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageURL} />
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>
+    );
+};
+
+export default VideoListItem;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import { API_KEY } from './config';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
-// Create a new component.
-// This component should produce some HTML
-const App = function() {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+      // this.setState({ videos: videos }); the same in ES6; only works, if the key and the value in object have the same name
+    }
+    );
+  }
+
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.videos[0]} />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
-
 
 // Take this component's genareted HTML an put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
